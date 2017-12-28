d58415fa4e5be3e1980aec5781c28f2a8e834ad6
DESKTOP-T315O7D\David <davidtopbing@gmail.com>
2017-12-28 18:36:28 +0700

    Part 10

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,47 +1,187 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
-router.get('/', (req, res, next)=>{
-    res.status(200).json({
-        message: 'Handling GET requests to /products'
+const Product = require("../models/product");
+
+router.get("/", (req, res, next) => {
+  Product.find()
+    // .select()
+    .select("name price _id")
+    .exec()
+    .then(docs => {
+      const response = {
+        count: docs.length,
+        products: docs.map(doc => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/products/" + doc._id
+            }
+          };
+        })
+      };
+      //   console.log(docs);
+      //
+      //   if (docs.length >= 0) {
+      res.status(200).json(response);
+      //   } else {
+      //     res.status(404).json({
+      //       message: "No entries found"
+      //     });
+      //   }
+      //   res.status(200).json(docs);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
+  //   res.status(200).json({
+  //     message: "Handling GET requests to /products"
+  //   });
 });
 
-router.post('/', (req, res, next)=>{
-    const product = {
-        name: req.body.name,
-        price: req.body.price
-    };
-    res.status(201).json({
-        message: 'Handling POST requests to /products',
-        createdProduct: product
+router.post("/", (req, res, next) => {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
+  });
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Created product success",
+        createdProduct: {
+          name: result.name,
+          price: result.price,
+          _id: result._id,
+          request: {
+            type: "GET",
+            url: "http://localhost:3000/products/" + result._id
+          }
+        }
+        // createdProduct: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
+
+  //   res.status(201).json({
+  //     message: "Handling POST requests to /products",
+  //     createdProduct: product
+  //   });
 });
 
-router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    if (id === 'special'){
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  Product.findById(id)
+    .select("name price _id")
+    .exec()
+    .then(doc => {
+      console.log("From database", doc);
+      if (doc) {
         res.status(200).json({
-            message: 'You discovered the special ID',
-            id: id
+          product: doc,
+          request: {
+            type: "GET",
+            description: "GET_ALL_PRODUCT",
+            url: "http://localhost:3000/products"
+          }
         });
-    } else {
-        res.status(200).json({
-            message: 'You passed an ID'
-        });
-    }
+        // res.status(200).json(doc);
+      } else {
+        res
+          .status(404)
+          .json({ message: "No valid entry found for provided ID" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+
+  //   if (id === "special") {
+  //     res.status(200).json({
+  //       message: "You discovered the special ID",
+  //       id: id
+  //     });
+  //   } else {
+  //     res.status(200).json({
+  //       message: "You passed an ID"
+  //     });
+  //   }
 });
 
-router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated product!'
+router.patch("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  const updateOps = {};
+  // Looped so only change the necessary object
+  // The PATCH request will be not iterable because its need to be passed as an object
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      //   console.log(result);
+      //   res.status(200).json(result);
+      res.status(200).json({
+        message: "Product updated",
+        request: {
+          type: "GET",
+          url: "http://localhost:3000/products/" + id
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
+  //   res.status(200).json({
+  //     message: "Updated product!"
+  //   });
 });
 
-router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted product!'
+router.delete("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  Product.remove({ _id: id })
+    .exec()
+    .then(result => {
+      //   res.status(200).json(result);
+      res.status(200).json({
+        message: "Product deleted!",
+        request: {
+          type: "POST",
+          url: "http://localhost:3000/products",
+          body: {
+            name: "String",
+            price: "Number"
+          }
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
+  //   res.status(200).json({
+  //     message: "Deleted product!"
+  //   });
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,49 +1,58 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
 
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  "mongodb://mousearts:" +
+    process.env.MONGO_ATLAS_PW +
+    "@node-learn-shard-00-00-uss3v.mongodb.net:27017,node-learn-shard-00-01-uss3v.mongodb.net:27017,node-learn-shard-00-02-uss3v.mongodb.net:27017/test?ssl=true&replicaSet=node-learn-shard-0&authSource=admin",
+  {
+    useMongoClient: true
+  }
+);
 
 // Use morgan first
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({encoded: false, extended: true}));
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ encoded: false, extended: true }));
 app.use(bodyParser.json());
 
-
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Controll-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    if (req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next();
-
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Controll-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
 });
 
 // Route which should handle requests
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
 
 app.use((req, res, next) => {
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
+  res.status(error.status || 500);
+
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
 });
 
 // app.use((req, res, next) => {
